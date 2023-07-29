14582bd479eb62643ec977257fa4114439e3c1c2
Solll Monsalve Betancourth <solanyimonsalve25@gmail.com>
2023-07-28 22:23:40 -0500

    se actualizan los datos

--- JavaScript/ejercicio2.js/js/script2.js
+++ JavaScript/ejercicio2.js/js/script2.js
@@ -11,8 +11,38 @@ alert("el nuevo sueldo con el aumento es: "+nsueldo);*/
 recibe una comision del 30% del total de las ventas del mes, 
 el quiere saber cuanto ganara en un mes que tuvo tres ventas*/
 
-const sueldo=1300000
+/*const sueldo=1300000
 const ventas=prompt("valor total de ventas");
 const pventas=30/100;
 const nsueldo=parseInt(sueldo) + parseInt(ventas*pventas)
-alert("el nuevo sueldo con el aumento del 30% de las ventas es: "+sueldo)
+alert("el nuevo sueldo con el aumento del 30% de las ventas es: "+sueldo)*/
+
+
+
+/*ejercicio 4 - . Una tienda ofrece un 25% de descuento por cada compra. Un cliente desea
+saber cuánto deberá pagar por su compra.*/
+
+/*let compra=prompt("ingrese valor total de la compra");
+let desc=25*100;
+let total=parseInt(compra) + parseInt(desc)
+alert("Con un descuento del 25% En su compra, el cliente debera pagar en total: "+desc)*/
+
+
+
+/*ejercicio 5 - Suponga que un individuo desea invertir su capital en un banco y desea saber
+cuánto dinero ganara después de un mes si el banco paga a razón de 2%
+mensual.*/
+
+
+
+
+/*ejercicio 6 - Un alumno desea saber cuál será su calificación final en la materia de
+Algoritmos. Dicha calificación se compone de los siguientes porcentajes:
+a. 55% del promedio de sus tres calificaciones parciales.
+b. 30% de la calificación del examen final.
+c. 15% de la calificación de un trabajo final.*/
+
+
+/*El dueño de una tienda compra un artículo a un precio determinado. Obtener el
+precio en que lo debe vender para obtener una ganancia del 30%.*/
+
